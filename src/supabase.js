5b9a4e27e622c8d0b75b7ddd1eabc8ad68559ba6
atulhpcapp-blip <atulhpcapp@gmail.com>
2026-05-04 12:56:33 +0530

    Add files via upload

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 const URL  = 'https://wlgbhrmycequuiabpwqf.supabase.co'
-const KEY  = 'sb_publishable_1I_V4RUqeSpzu7d0NXlhVg_z4rs0UbZ'
+const KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsZ2Jocm15Y2VxdXVpYWJwd3FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxODIwMjAsImV4cCI6MjA5Mjc1ODAyMH0.9cDZdZufK0EcWWjIpqp3gLPlwfKVvs5LEOLnw8wM4dw'
 export const supabase = createClient(URL, KEY)
