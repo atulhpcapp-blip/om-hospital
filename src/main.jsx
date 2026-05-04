e827d5f5ba82dd526405721c9752b4d8f9902163
atulhpcapp-blip <atulhpcapp@gmail.com>
2026-05-04 12:11:20 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,2 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
+// App renders itself via createRoot in App.jsx
+export default function main(){}
