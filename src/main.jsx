4401d8b6bdc4c9b9b0d7a31e72cfd99a485aa341
atulhpcapp-blip <atulhpcapp@gmail.com>
2026-05-06 09:23:32 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,1 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(<App/>)
+// App renders itself via createRoot in App.jsx
